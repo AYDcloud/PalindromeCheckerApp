ee780ff454479f4fb7cca05a003ab4f54a896594
AYDcloud <ayushdalela4903@gmail.com>
2026-02-19 09:04:07 +0530

    UC1: Intro

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args)
+
+    {
+System.out.println("Welcome to Palindrome Checker");
+        System.out.println("Made by Ayush Dalela");
+    }
+}
