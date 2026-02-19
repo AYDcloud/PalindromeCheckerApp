12693db64770342773a575feb6793e2e7326cca3
AYDcloud <ayushdalela4903@gmail.com>
2026-02-19 09:27:26 +0530

    UC2:Hardcoded palindrome

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = scanner.nextLine();
+
+        int left = 0;
+        int right = text.length() - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + text + "\" is NOT a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
