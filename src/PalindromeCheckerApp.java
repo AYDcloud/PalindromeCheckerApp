fea6a59a43eab7b795b5722c081299c38ca94a56
AYDcloud <ayushdalela4903@gmail.com>
2026-02-26 09:42:17 +0530

    Implemented UC8

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,41 +1,58 @@
 import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) { this.data = data; }
+    }
+
+    static Node reverse(Node head) {
+        Node prev = null, curr = head, next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String text = scanner.nextLine();
+        String s = sc.nextLine();
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < text.length(); i++) {
-            char ch = text.charAt(i);
-            queue.add(ch);
-            stack.push(ch);
+        Node head = null, tail = null;
+        for (int i = 0; i < s.length(); i++) {
+            Node newNode = new Node(s.charAt(i));
+            if (head == null) head = tail = newNode;
+            else { tail.next = newNode; tail = newNode; }
         }
 
-        boolean isPalindrome = true;
-
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
-            System.out.println("The string \"" + text + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + text + "\" is NOT a palindrome.");
-        }
-
-        scanner.close();
+        System.out.println("The string \"" + s + "\" " +
+                (isPalindrome(head) ? "is a palindrome." : "is NOT a palindrome."));
+        sc.close();
     }
 }
